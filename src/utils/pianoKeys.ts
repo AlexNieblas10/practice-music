102ab691d2c19be352e31505cc24cbc79e52f493
AlexNieblas10 <elalexnieblas@gmail.com>
2024-02-06 11:17:17 -0700

    UPDATE pianokeys

--- src/utils/pianoKeys.ts
+++ src/utils/pianoKeys.ts
@@ -1,16 +1,16 @@
 import { Piano, BlackOrWhite } from "../../interfacesAndTypes"
 
 export const pianoKeys: Piano = [
-	{ name: "C", blackOrWhite: BlackOrWhite.WHITE, number: 1, selected: false },
-	{ name: "C#", blackOrWhite: BlackOrWhite.BLACK, number: 2, selected: false },
-	{ name: "D", blackOrWhite: BlackOrWhite.WHITE, number: 3, selected: false },
-	{ name: "D#", blackOrWhite: BlackOrWhite.BLACK, number: 4, selected: false },
-	{ name: "E", blackOrWhite: BlackOrWhite.WHITE, number: 5, selected: false },
-	{ name: "F", blackOrWhite: BlackOrWhite.WHITE, number: 6, selected: false },
-	{ name: "F#", blackOrWhite: BlackOrWhite.BLACK, number: 7, selected: false },
-	{ name: "G", blackOrWhite: BlackOrWhite.WHITE, number: 8, selected: false },
-	{ name: "G#", blackOrWhite: BlackOrWhite.BLACK, number: 9, selected: false },
-	{ name: "A", blackOrWhite: BlackOrWhite.WHITE, number: 10, selected: false },
-	{ name: "A#", blackOrWhite: BlackOrWhite.BLACK, number: 11, selected: false },
-	{ name: "B", blackOrWhite: BlackOrWhite.WHITE, number: 12, selected: false },
+	{ name: "C", blackOrWhite: BlackOrWhite.WHITE, number: 1, selected: null },
+	{ name: "C#", blackOrWhite: BlackOrWhite.BLACK, number: 2, selected: null },
+	{ name: "D", blackOrWhite: BlackOrWhite.WHITE, number: 3, selected: null },
+	{ name: "D#", blackOrWhite: BlackOrWhite.BLACK, number: 4, selected: null },
+	{ name: "E", blackOrWhite: BlackOrWhite.WHITE, number: 5, selected: null },
+	{ name: "F", blackOrWhite: BlackOrWhite.WHITE, number: 6, selected: null },
+	{ name: "F#", blackOrWhite: BlackOrWhite.BLACK, number: 7, selected: null },
+	{ name: "G", blackOrWhite: BlackOrWhite.WHITE, number: 8, selected: null },
+	{ name: "G#", blackOrWhite: BlackOrWhite.BLACK, number: 9, selected: null },
+	{ name: "A", blackOrWhite: BlackOrWhite.WHITE, number: 10, selected: null },
+	{ name: "A#", blackOrWhite: BlackOrWhite.BLACK, number: 11, selected: null },
+	{ name: "B", blackOrWhite: BlackOrWhite.WHITE, number: 12, selected: null },
 ]
