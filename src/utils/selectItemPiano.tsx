abec08fbb1a22bf42a211646e18c5f1eed2eb873
AlexNieblas10 <elalexnieblas@gmail.com>
2024-02-06 11:17:35 -0700

    UPDATE selectItem

--- src/utils/selectItemPiano.tsx
+++ src/utils/selectItemPiano.tsx
@@ -2,10 +2,10 @@ import { BlackOrWhite, PianoKey } from "../../interfacesAndTypes"
 import { BlackKey } from "../components/BlackKey"
 import { WhiteKey } from "../components/WhiteKey"
 
-export function selectItemPiano(pianoKey:PianoKey){
-  if (pianoKey.blackOrWhite === BlackOrWhite.WHITE) {
-    return <WhiteKey key={pianoKey.name} />
-  } else {
-    return <BlackKey key={pianoKey.name} />
-  }
+export function selectItemPiano(pianoKey: PianoKey) {
+	if (pianoKey.blackOrWhite === BlackOrWhite.WHITE) {
+		return <WhiteKey key={pianoKey.name} pianoInfo={pianoKey} />
+	} else {
+		return <BlackKey key={pianoKey.name} pianoInfo={pianoKey} />
+	}
 }
