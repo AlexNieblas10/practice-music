29b5f8ee3d71772edafb25d2dff4a9e1d2ecff59
AlexNieblas10 <elalexnieblas@gmail.com>
2024-02-21 11:52:05 -0700

    UPDATE add context in the router

--- routes.tsx
+++ routes.tsx
@@ -1,6 +1,14 @@
 import { createBrowserRouter } from "react-router-dom"
 import { GuessTheNote } from "./src/pages/GuessTheNote"
+import { GuessNoteContext } from "./src/context/GuessNote"
 
 export const routes = createBrowserRouter([
-  { path: "/guessnote", element: <GuessTheNote /> }
+	{
+		path: "/guessnote",
+		element: (
+			<GuessNoteContext>
+				<GuessTheNote />
+			</GuessNoteContext>
+		),
+	},
 ])
